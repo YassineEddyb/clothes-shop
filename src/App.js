91de97ac3acfad1964c63640823602f5fa6db8b3
Yassine ed-dyb <yassineeddyb@gmail.com>
2021-04-08 17:16:50 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+import HomePage from "./homepage-menu.js";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/homepage-menu.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import "./homepage-styles.scss";
+
+function Homepagemenu() {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hats</h1>
+            <span className="subtitle">see more</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">jackets</h1>
+            <span className="subtitle">see more</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">test</h1>
+            <span className="subtitle">see more</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hats</h1>
+            <span className="subtitle">see more</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hats</h1>
+            <span className="subtitle">see more</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Homepagemenu;
